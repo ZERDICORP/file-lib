22b8a43b556ade97d5696b8b4557922b6c3a5010
ZERDICORP <56264511+ZERDICORP@users.noreply.github.com>
2021-07-16 00:28:37 +0300

    minor syntax edits

--- file.h
+++ file.h
@@ -14,84 +14,85 @@
 #ifndef ZER__FILE_MODEL
 	#define ZER__FILE_MODEL
 	#define mWARNING(sMessage) (std::cerr << "[warning]: " << sMessage << ".." << std::endl)
-		namespace zer
+
+	namespace zer
+	{
+		namespace file
 		{
-			namespace file
+			struct Info
 			{
-				struct Info
-				{
-					std::string sPath;
-					std::string sFullName;
-					std::string sName;
-					std::string sFormat;
+				std::string sPath;
+				std::string sFullName;
+				std::string sName;
+				std::string sFormat;
 
-					int iLength;
-				};
-
-				struct Modifier
-				{
-					const static int standard = 0;
-					const static int lines = 1;
-					const static int binary = 2;
-					const static int hidden = 3;
-				};
+				int iLength;
 			};
 
-			class File
+			struct Modifier
 			{
-				private:
-					std::fstream _fs;
-
-					bool _bDoesExists;
-					inline bool _bHasModifier(std::initializer_list<int> modifiers, int iDesiredModifier);
-					inline bool _doesExists(std::string sFilePath);
-
-					int _iSliceSize = 0;
-					int _iLength = 0;
-					int _iSlicesLen = 0;
-
-					std::string _sPath;
-					std::string _sFullName;
-					std::string _sName;
-					std::string _sFormat;
-					std::string _sData;
-
-					zer::row<std::string> _rLines;
-
-					inline void _setFilePath(std::string sFilePath);
-					inline void _write(std::string sData);
-					inline void _read();
-					inline void _readLines();
-					inline void _slice(int iSliceIndex);
-
-				public:
-					inline File() {}
-					inline File(std::string sFilePath) {this -> open(sFilePath);}
-					inline ~File() {if (this -> isOpen()) this -> _fs.close();}
-
-					void close() {this -> _fs.close();}
-
-					int linesLen() {return this -> _rLines.len();}
-					int const& len() {return this -> _iLength;}
-					int const& slicesLen() {return this -> _iSlicesLen;}
-
-					bool doesExists() {return this -> _bDoesExists;}
-					bool isOpen() {return this -> _fs.is_open();}
-					bool doesExists(std::string sFilePath) {return this -> _doesExists(sFilePath);}
-
-					inline void open(std::string sFilePath);
-					inline void write(std::string sData, std::string sFilePath, std::initializer_list<int> modifiers = {file::Modifier::standard});
-					inline void write(std::string sData, std::initializer_list<int> modifiers = {file::Modifier::standard});
-					inline void read(std::string sFilePath, std::initializer_list<int> modifiers = {file::Modifier::standard});
-					inline void read(std::initializer_list<int> modifiers = {file::Modifier::standard});
-					inline void setSliceSize(int iSliceSize);
-					inline void slice(int iSliceIndex, std::initializer_list<int> modifiers);
-
-					std::string const& data() {return this -> _sData;}
-					std::string const& line(int iIndex) {return this -> _rLines[iIndex];}
-
-					file::Info info() {return file::Info{this -> _sPath, this -> _sFullName, this -> _sName, this -> _sFormat, this -> _iLength};}
+				const static int standard = 0;
+				const static int lines = 1;
+				const static int binary = 2;
+				const static int hidden = 3;
 			};
-			#include "file.inl"
 		};
+
+		class File
+		{
+			private:
+				std::fstream _fs;
+
+				bool _bDoesExists;
+				inline bool _bHasModifier(std::initializer_list<int> modifiers, int iDesiredModifier);
+				inline bool _doesExists(std::string sFilePath);
+
+				int _iSliceSize = 0;
+				int _iLength = 0;
+				int _iSlicesLen = 0;
+
+				std::string _sPath;
+				std::string _sFullName;
+				std::string _sName;
+				std::string _sFormat;
+				std::string _sData;
+
+				zer::row<std::string> _rLines;
+
+				inline void _setFilePath(std::string sFilePath);
+				inline void _write(std::string sData);
+				inline void _read();
+				inline void _readLines();
+				inline void _slice(int iSliceIndex);
+
+			public:
+				inline File() {}
+				inline File(std::string sFilePath) {this -> open(sFilePath);}
+				inline ~File() {if (this -> isOpen()) this -> _fs.close();}
+
+				void close() {this -> _fs.close();}
+
+				int linesLen() {return this -> _rLines.len();}
+				int const& len() {return this -> _iLength;}
+				int const& slicesLen() {return this -> _iSlicesLen;}
+
+				bool doesExists() {return this -> _bDoesExists;}
+				bool isOpen() {return this -> _fs.is_open();}
+				bool doesExists(std::string sFilePath) {return this -> _doesExists(sFilePath);}
+
+				inline void open(std::string sFilePath);
+				inline void write(std::string sData, std::string sFilePath, std::initializer_list<int> modifiers = {file::Modifier::standard});
+				inline void write(std::string sData, std::initializer_list<int> modifiers = {file::Modifier::standard});
+				inline void read(std::string sFilePath, std::initializer_list<int> modifiers = {file::Modifier::standard});
+				inline void read(std::initializer_list<int> modifiers = {file::Modifier::standard});
+				inline void setSliceSize(int iSliceSize);
+				inline void slice(int iSliceIndex, std::initializer_list<int> modifiers);
+
+				std::string const& data() {return this -> _sData;}
+				std::string const& line(int iIndex) {return this -> _rLines[iIndex];}
+
+				file::Info info() {return file::Info{this -> _sPath, this -> _sFullName, this -> _sName, this -> _sFormat, this -> _iLength};}
+		};
+		#include "file.inl"
+	};
 #endif
